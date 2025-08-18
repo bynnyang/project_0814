a8b2cf8cadc18a7e89909228d7828a475549a4a5
bynn.yang <bynn.yang@nio.com>
2025-08-18 14:14:05 +0800

    train_model

---
+++ train.py
@@ -0,0 +1,127 @@
+import argparse
+
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from dataset_interface.dataloader import ParkingDataloaderModule
+from dataset_interface.dataset_real import ParkingDataModuleReal
+from utils.config import get_train_config_obj
+from dataset import GraphData
+from dataset import GraphDataset
+from torch.utils.data import DataLoader
+from model_interface.model_interface import get_parking_model, setup_callbacks
+import torch
+import torch.optim as optim
+import os
+import time
+from loss.traj_point_loss import TokenTrajPointLoss
+from utils.eval import get_eval_metric_results
+from model_interface.model.parking_model_real import ParkingModelReal
+
+
+decay_lr_factor = 0.3
+decay_lr_every = 5
+lr = 0.001
+epochs = 25
+end_epoch = 0
+lr = 0.001
+in_channels, out_channels = 8, 60
+show_every = 20
+val_every = 5
+best_minade = float('inf')
+save_dir = './trained_params'
+date_record = "250815"
+
+
+def save_checkpoint(checkpoint_dir, model, optimizer, end_epoch, val_minade, date):
+    # state_dict: a Python dictionary object that:
+    # - for a model, maps each layer to its parameter tensor;
+    # - for an optimizer, contains info about the optimizerâ€™s states and hyperparameters used.
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    state = {
+        'state_dict': model.state_dict(),
+        'optimizer' : optimizer.state_dict(),
+        'end_epoch' : end_epoch,
+        'val_minade': val_minade
+        }
+    checkpoint_path = os.path.join(checkpoint_dir, f'epoch_{end_epoch}.valminade_{val_minade:.3f}.{date}.{"xkhuang"}.pth')
+    torch.save(state, checkpoint_path)
+    print('model saved to %s' % checkpoint_path)
+    
+def load_checkpoint(checkpoint_path, model, optimizer):
+    state = torch.load(checkpoint_path)
+    model.load_state_dict(state['state_dict'])
+    optimizer.load_state_dict(state['optimizer'])
+    print('model loaded from %s' % checkpoint_path)
+    return checkpoint_path['end_epoch']
+def train(config_obj):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+   
+    dataset = ParkingDataModuleReal(config_obj, is_train=1)
+    train_loader = DataLoader(dataset, batch_size= config_obj.batch_size, shuffle=True, num_workers=config_obj.num_workers)
+    val_loader = DataLoader(dataset, batch_size= config_obj.batch_size, shuffle=True, num_workers=config_obj.num_workers)
+
+    model = ParkingModelReal(config_obj)
+    model = model.to(device=device)
+    traj_point_loss_func = TokenTrajPointLoss(config_obj)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = optim.lr_scheduler.StepLR(
+        optimizer, step_size=decay_lr_every, gamma=decay_lr_factor)
+    
+
+
+    # training loop
+    model.train()
+    for epoch in range(epochs):
+        print(epoch)
+        acc_loss = .0
+        num_samples = 0
+        start_tic = time.time()
+        for data in train_loader:
+            data.to(device)
+            if epoch < end_epoch: break
+            optimizer.zero_grad()
+            out = model(data)
+            loss = traj_point_loss_func(out, data)
+            loss.backward()
+            acc_loss += config_obj.batch_size * loss.item()
+            num_samples += data["gt_traj_point_token"].shape[0]
+            optimizer.step()
+            global_step += 1
+            if (global_step + 1) % show_every == 0:
+                print( f"loss at epoch {epoch} step {global_step}:{loss.item():3f}, lr:{optimizer.state_dict()['param_groups'][0]['lr']: .6f}, time:{time.time() - start_tic: 4f}sec")
+        scheduler.step()
+        print(
+            f"loss at epoch {epoch}:{acc_loss / num_samples:.3f}, lr:{optimizer.state_dict()['param_groups'][0]['lr']: .6f}, time:{time.time() - start_tic: 4f}sec")
+        
+        if (epoch+1) % val_every == 0 and (not epoch < end_epoch):
+            print("eval as epoch:{epoch}")
+            metrics = get_eval_metric_results(config_obj, model, val_loader, device)
+            curr_minade = metrics["minADE"]
+            print(f"minADE:{metrics['minADE']:3f}, minFDE:{metrics['minFDE']:3f}, MissRate:{metrics['MR']:3f}")
+
+            if curr_minade < best_minade:
+                best_minade = curr_minade
+                save_checkpoint(save_dir, model, optimizer, epoch, best_minade, date_record)
+                
+    # eval result on the identity dataset
+    metrics = get_eval_metric_results(config_obj, model, val_loader, device)
+    curr_minade = metrics["minADE"]
+    if curr_minade < best_minade:
+        best_minade = curr_minade
+        save_checkpoint(save_dir, model, optimizer, -1, best_minade, date_record)
+
+
+def main():
+    seed_everything(16)
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--config', default='./config/training_real.yaml', type=str)
+    args = arg_parser.parse_args()
+    config_path = args.config
+    config_obj = get_train_config_obj(config_path)
+
+    train(config_obj)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/eval.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020-06-18 22:00
+# @Author  : Xiaoke Huang
+# @Email   : xiaokehuang@foxmail.com
+import torch
+from utils.metrics import CustomizedMetric
+from pprint import pprint
+from typing import List
+from loss.traj_point_loss import TokenTrajPointLoss
+
+
+def get_eval_metric_results(config_obj, model, data_loader, device):
+ 
+    model.eval()
+    traj_point_loss_func = TokenTrajPointLoss(config_obj)
+    with torch.no_grad():
+        loss = 0
+        for data in data_loader:
+            data = data.to(device)
+
+            out = model(data)
+            loss+=traj_point_loss_func(out, data)
+        return loss
+
+def eval_loss():
+    raise NotImplementedError("not finished yet")
+    model.eval()
+    from utils.viz_utils import show_pred_and_gt
+    with torch.no_grad():
+        accum_loss = .0
+        for sample_id, data in enumerate(train_loader):
+            data = data.to(device)
+            gt = data.y.view(-1, out_channels).to(device)
+            optimizer.zero_grad()
+            out = model(data)
+            loss = F.mse_loss(out, gt)
+            accum_loss += batch_size * loss.item()
+            print(f"loss for sample {sample_id}: {loss.item():.3f}")
+
+            for i in range(gt.size(0)):
+                pred_y = out[i].numpy().reshape((-1, 2)).cumsum(axis=0)
+                y = gt[i].numpy().reshape((-1, 2)).cumsum(axis=0)
+                show_pred_and_gt(pred_y, y)
+                plt.show()
+        print(f"eval overall loss: {accum_loss / len(ds):.3f}")
